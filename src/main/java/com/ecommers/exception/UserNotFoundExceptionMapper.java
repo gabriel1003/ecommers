d6d14ab953470229719cbe67e4626feee54a21f5
gabriel1003 <gabriel.samersla97@gmail.com>
2025-02-05 10:29:09 -0300

    feat: Melhorar tratamento de erro para UserNotFoundException

--- src/main/java/com/ecommers/exception/UserNotFoundExceptionMapper.java
+++ src/main/java/com/ecommers/exception/UserNotFoundExceptionMapper.java
@@ -7,8 +7,16 @@ import jakarta.ws.rs.ext.Provider;
 @Provider
 public class UserNotFoundExceptionMapper implements ExceptionMapper<UserNotFoundException> {
 
+    private static final String DEFAULT_MESSAGE = "Usuário não encontrado.";
+
     @Override
     public Response toResponse(UserNotFoundException exception) {
-        return Response.status(Response.Status.NOT_FOUND.getStatusCode(), "User not found").build();
+        String message = (exception.getMessage() != null && !exception.getMessage().isEmpty()) ?
+                exception.getMessage() : DEFAULT_MESSAGE;
+
+        return Response.status(Response.Status.NOT_FOUND)
+                .entity(message)
+                .type("text/plain")
+                .build();
     }
 }
